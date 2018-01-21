610e2d38d0ffb1d4ae8fb1c1183791c944a2151f
deverzone <pichet.suwannarat@gmail.com>
2018-01-21 23:36:29 +0700

    Update hooker.php

--- hooker.php
+++ hooker.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] != 'message' && $event['message']['type'] != 'text') {
 
 			// mLAB
 			$mlab_key="cprtOzFzDYo8B7IRUwpZ5Evi4AazXrbl";
